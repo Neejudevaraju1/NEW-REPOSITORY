bb85cc76fe7b5d9dce6e3c6a58b19a9d0ce198c6
Neeju Devaraju <Neeju Devaraju@DESKTOP-N3G62FT>
2025-05-21 14:39:00 +0530

    commit 1

---
+++ Java_Project/src/New_package/vowels.java
@@ -0,0 +1,28 @@
+package New_package;
+
+public class vowels {
+
+	public static void main(String[] args) {
+		String alphabet = "b";
+		switch(alphabet) {
+		case "a":
+			System.out.println("the alphabet is a vowel");
+			break;
+		case "e":
+			System.out.println("the alphabet is a vowel");
+			break;
+		case "i":
+			System.out.println("the alphabet is a vowel");
+			break;
+		case "o":
+			System.out.println("the alphabet is a vowel");
+			break;
+		case "u":
+			System.out.println("the alphabet is a vowel");
+			break;
+		default:
+			System.out.println("the alphabet is not a vowel");
+		}
+	}
+
+}
