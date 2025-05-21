2dd3657d1c62c74e1b9f994b8ef4a08f682f2932
User <User@LAPTOP-VO7SSANT.bbrouter>
2025-05-21 21:43:23 +0530

    changes

--- Aggregation/src/main/java/Aggregation/Aggregate.java
+++ Aggregation/src/main/java/Aggregation/Aggregate.java
@@ -5,11 +5,12 @@ public class Aggregate {
 	public static void main(String[] args) {
 		Author Au = new Author("JK ROWLING",56,"SCOTLAND");
 		Book bk =new Book("HARRY POTTER",999,Au);
-System.out.println(bk.Bname);
+		bk.display();
+/*System.out.println(bk.Bname);
 System.out.println(bk.price);
 System.out.println(bk.author.Name);
 System.out.println(bk.author.Age);
-System.out.println(bk.author.Place);
+System.out.println(bk.author.Place);*/
 	}
 
 }
--- Aggregation/src/main/java/Aggregation/Book.java
+++ Aggregation/src/main/java/Aggregation/Book.java
@@ -9,4 +9,11 @@ Book(String name,int price,Author author){
 	this.price=price;
 	this.author=author;
 }
+void display() {
+	System.out.println(Bname);
+	System.out.println(price);
+	System.out.println(author.Name);
+	System.out.println(author.Age);
+	System.out.println(author.Place);
+}
 }
