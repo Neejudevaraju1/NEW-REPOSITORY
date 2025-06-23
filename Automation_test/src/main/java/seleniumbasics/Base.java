099f266f75f250142dff790cf7503ad6f01a4f79
User <User@LAPTOP-VO7SSANT.bbrouter>
2025-06-23 21:34:58 +0530

    automation

---
+++ Automation_test/src/main/java/seleniumbasics/Base.java
@@ -0,0 +1,24 @@
+package seleniumbasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	WebDriver driver;
+	public void browserInitialisation() {
+		 driver= new ChromeDriver();
+		 driver.get("https://selenium.qabible.in/index.php");
+		 driver.manage().window().maximize();
+	}
+	public void quitAndClose() {
+		//driver.close()
+          driver.quit();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Base base=new Base();
+     base.browserInitialisation();
+     base.quitAndClose();
+	}
+
+}
